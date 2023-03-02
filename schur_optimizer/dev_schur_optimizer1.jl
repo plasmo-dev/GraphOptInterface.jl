59d95c911f841154c34b8d858dcb4643a29a05b3
jalving <jhjalving@gmail.com>
2023-03-02 10:25:14 -0800

    decided all edges should be completely local

---
+++ schur_optimizer/dev_schur_optimizer1.jl
@@ -0,0 +1,194 @@
+include("schur_optimizer.jl")
+
+using SparseArrays
+
+optimizer = SchurOptimizer()
+
+##################################################
+# node 1 and edge 1
+##################################################
+node1 = BOI.add_node!(optimizer, BOI.BlockIndex(0))
+x1 = MOI.add_variables(optimizer, node1, 3)
+edge1 = BOI.add_edge!(optimizer, BOI.BlockIndex(0), node1)
+
+c1 = [1.0, 2.0, 3.0]
+w1 = [0.3, 0.5, 1.0]
+C1 = 3.2
+
+# set edge1 objective
+MOI.set(
+	edge1,
+	MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+	MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(c1, x1), 0.0),
+)
+MOI.set(edge1, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+
+# add edge1 constraint
+ci = MOI.add_constraint(
+    optimizer,
+    edge1,
+    MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(w1, x1), 0.0),
+    MOI.LessThan(C1),
+)
+
+# add edge1 nonlinear constraint
+nlp1 = MOI.Nonlinear.Model()
+MOI.Nonlinear.add_constraint(nlp1, :(1 + sqrt($(x1[1]))), MOI.LessThan(2.0))
+evaluator1 = MOI.Nonlinear.Evaluator(nlp1, MOI.Nonlinear.SparseReverseMode(), x1)
+MOI.initialize(evaluator1, [:Grad, :Jac, :Hess])
+block1 = MOI.NLPBlockData(evaluator1)
+MOI.set(edge1, MOI.NLPBlock(), block1)
+
+# evaluate edge 1 NLP functions
+# objective
+x1_eval = [1.0, 1.0, 1.0]
+edge1_obj = MOI.eval_objective(edge1, x1_eval)
+
+# constraints
+g1_eval = zeros(2)
+MOI.eval_constraint(edge1, g1_eval, x1_eval)
+
+# gradient
+grad1_eval = zeros(3)
+MOI.eval_objective_gradient(edge1, grad1_eval, x1_eval)
+
+# jacobian
+jac_structure1 = MOI.jacobian_structure(edge1)
+jac_values1 = zeros(length(jac_structure1))
+MOI.eval_constraint_jacobian(edge1, jac_values1, x1_eval)
+
+# hessian
+hess_lag_structure1 = MOI.hessian_lagrangian_structure(edge1)
+hess_values1 = zeros(length(hess_lag_structure1))
+MOI.eval_hessian_lagrangian(edge1, hess_values1, x1_eval, 1.0, ones(2))
+
+
+##################################################
+# node 2 and edge 2
+##################################################
+node2 = BOI.add_node!(optimizer, BOI.BlockIndex(0))
+
+# NOTE: returns optimizer index, not node index
+x2 = MOI.add_variables(optimizer, node2, 3)
+edge2 = BOI.add_edge!(optimizer, BOI.BlockIndex(0), node2)
+
+c2 = [2.0, 3.0, 4.0]
+w2 = [0.2, 0.1, 1.2]
+C2 = 2.0
+
+MOI.set(
+	edge2,
+	MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
+	MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(c2, x2), 0.0),
+)
+MOI.set(edge2, MOI.ObjectiveSense(), MOI.MAX_SENSE)
+MOI.add_constraint(
+    optimizer, 
+    edge2,
+    MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(w2, x2), 0.0),
+    MOI.LessThan(C2)
+)
+
+nlp2 = MOI.Nonlinear.Model()
+
+MOI.Nonlinear.add_constraint(nlp2, :(1 + sqrt($(x2[2]))), MOI.LessThan(3.0))
+evaluator2 = MOI.Nonlinear.Evaluator(nlp2, MOI.Nonlinear.SparseReverseMode(), x2)
+MOI.initialize(evaluator2, [:Grad, :Jac, :Hess])
+block2 = MOI.NLPBlockData(evaluator2)
+MOI.set(edge2, MOI.NLPBlock(), block2)
+
+# evaluate edge 2 NLP functions
+# objective
+x2_eval = [1.0, 1.0, 1.0]
+edge2_obj = MOI.eval_objective(edge2, x2_eval)
+
+# constriants
+g2_eval = zeros(2)
+MOI.eval_constraint(edge2, g2_eval, x2_eval)
+
+# gradient
+grad2_eval = zeros(3)
+MOI.eval_objective_gradient(edge2, grad2_eval, x2_eval)
+
+# jacobian
+jac_structure2 = MOI.jacobian_structure(edge2)
+jac_values2 = zeros(length(jac_structure2))
+MOI.eval_constraint_jacobian(edge2, jac_values2, x2_eval)
+
+# hessian
+hess_lag_structure2 = MOI.hessian_lagrangian_structure(edge2)
+hess_values2 = zeros(length(hess_lag_structure2))
+MOI.eval_hessian_lagrangian(edge2, hess_values2, x2_eval, 1.0, ones(2))
+
+##################################################
+# edge 3 - couple node1 and node2
+##################################################
+edge3 = BOI.add_edge!(optimizer, BOI.BlockIndex(0), (node1, node2))
+
+x1_node = BOI.block_index.(Ref(node1), x1)
+x2_node = BOI.block_index.(Ref(node2), x2)
+
+MOI.add_constraint(
+    optimizer,   
+    edge3,
+    MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([1.0,-1.0], [x1_node[1],x2_node[1]]), 0.0),
+    MOI.EqualTo(0.0)
+)
+nlp3 = MOI.Nonlinear.Model()
+MOI.Nonlinear.add_constraint(nlp3, :(1 + sqrt($(x1_node[1])) + $(x2_node[3])^3), MOI.LessThan(5.0))
+evaluator3 = MOI.Nonlinear.Evaluator(nlp3, MOI.Nonlinear.SparseReverseMode(), [x1_node;x2_node])
+MOI.initialize(evaluator3, [:Grad, :Jac, :Hess])
+block3 = MOI.NLPBlockData(evaluator3)
+MOI.set(edge3, MOI.NLPBlock(), block3)
+
+# evaluate edge 3 NLP functions
+# specify a sparse vector of variable indices and values
+x3_eval = sparsevec([1, 4, 6], [1.0, 1.0, 1.0])
+
+# objective
+edge3_obj = MOI.eval_objective(edge3, x3_eval)
+
+# constraints
+g3_eval = zeros(2)
+MOI.eval_constraint(edge3, g3_eval, x3_eval)
+
+# gradient
+grad3_eval = zeros(6)
+MOI.eval_objective_gradient(edge3, grad3_eval, x3_eval)
+
+# jacobian
+jac_structure3 = MOI.jacobian_structure(edge3)
+jac_values3 = zeros(length(jac_structure3))
+MOI.eval_constraint_jacobian(edge3, jac_values3, x3_eval)
+
+# hessian
+hess_lag_structure3 = MOI.hessian_lagrangian_structure(edge3)
+hess_values3 = zeros(length(hess_lag_structure3))
+MOI.eval_hessian_lagrangian(edge3, hess_values3, x3_eval, 1.0, ones(2))
+
+##################################################
+# Block evaluator
+##################################################
+
+# block_evaluator = BOI.BlockEvaluator(optimizer.block)
+
+# x_block = ones(6)
+
+# # objective
+# block_obj = MOI.eval_objective(block_evaluator, x_block)
+
+# # constraints
+# g_block = zeros(6)
+# MOI.eval_constraint(block_evaluator, g_block, x_block)
+
+# # gradient
+# grad_block = zeros(6)
+# MOI.eval_objective_gradient(block_evaluator, grad_block, x_block)
+
+# jac_structure_block = MOI.jacobian_structure(block_evaluator)
+# jac_values_block = zeros(length(jac_structure_block))
+# MOI.eval_constraint_jacobian(block_evaluator, jac_values_block, x_block)
+
+# hess_lag_structure3 = MOI.hessian_lagrangian_structure(edge3)
+# hess_values3 = zeros(length(hess_lag_structure3))
+# MOI.eval_hessian_lagrangian(edge3, hess_values3, x3_eval, 1.0, ones(2))
--- src/block_nlp_evaluator.jl
+++ src/block_nlp_evaluator.jl
@@ -14,7 +14,7 @@ end
 Create `Evaluator`, a subtype of `MOI.AbstractNLPEvaluator`, from `Model`.
 """
 mutable struct BlockEvaluator{B} <: MOI.AbstractNLPEvaluator
-    # The internal datastructure.
+    # The block containing nodes and edges
     block::Block
     
     # The abstract-differentiation backend
@@ -22,6 +22,10 @@ mutable struct BlockEvaluator{B} <: MOI.AbstractNLPEvaluator
 
     # edge data for evaluations
     edge_data::OrderedDict{Edge,EdgeData}
+    num_variables::Int64
+    num_constraints::Int64
+    nnz_hess::Int64
+    nnz_jac::Int64
 
     eval_objective_timer::Float64
     eval_constraint_timer::Float64
@@ -36,7 +40,6 @@ mutable struct BlockEvaluator{B} <: MOI.AbstractNLPEvaluator
 
         edge_data = Dict{Edge,EdgeData}()
 
-        # count_variables = 0
         count_constraints = 0
         count_nnzh = 0
         count_nnzj = 0
@@ -45,35 +48,38 @@ mutable struct BlockEvaluator{B} <: MOI.AbstractNLPEvaluator
             nlp_data = MOI.get(edge, MOI.NLPBlock())
             MOI.initialize(nlp_data.evaluator, [:Grad,:Hess,:Jac])
             
-
-            # variable indices
-            # n_var_edge = num_variables(edge)
-            # ninds = count_variables+1 : count_variables+n_var_edge
-            # count_variables += n_var_edge
+            # edge columns
             columns = column_inds(edge)
             ninds = range(columns[1],columns[end])
 
+            # edge rows
             n_con_edge = num_constraints(edge)
             minds = count_constraints+1 : count_constraints+n_con_edge
             count_constraints += n_con_edge
 
-            # hessian indices
+            # edge hessian indices
             hessian_sparsity = MOI.hessian_lagrangian_structure(edge)
             nnzs_hess_inds = count_nnzh+1 : count_nnzh + length(hessian_sparsity)
             count_nnzh += length(hessian_sparsity)
 
-            # jacobian indices
+            # edge jacobian indices
             jacobian_sparsity = MOI.jacobian_structure(edge)
             nnzs_jac_inds = count_nnzj+1 : count_nnzj + length(jacobian_sparsity)
             count_nnzj += length(jacobian_sparsity)
 
-            edge_data[edge] = EdgeData(ninds, minds, nnzs_hess_inds, nnzs_jac_inds)
+            edge_data[edge] = EdgeData(ninds, minds, nnzs_jac_inds, nnzs_hess_inds)
         end
+
+        num_variables = MOI.get(block, MOI.NumberOfVariables())
 
         return new{B}(
             block,
             backend,
             edge_data,
+            num_variables,
+            count_constraints,
+            count_nnzh,
+            count_nnzj,
             0.0,
             0.0,
             0.0,
@@ -81,21 +87,6 @@ mutable struct BlockEvaluator{B} <: MOI.AbstractNLPEvaluator
             0.0,
         )
     end
-end
-
-
-function num_variables(node::Node)
-    return MOI.get(node, MOI.NumberOfVariables())
-end
-
-function num_constraints(edge::Edge)
-    n_con = 0
-    for (F,S) in MOI.get(edge, MOI.ListOfConstraintTypesPresent())
-        n_con += MOI.get(edge, MOI.NumberOfConstraints{F,S}())
-    end
-    nlp_block = MOI.get(edge, MOI.NLPBlock())
-    n_con += length(nlp_block.constraint_bounds)
-    return n_con
 end
 
 function MOI.initialize(evaluator::BlockEvaluator, requested_features::Vector{Symbol})
@@ -118,25 +109,95 @@ end
 ### Eval_Grad_F_CB
 
 function MOI.eval_objective_gradient(evaluator::BlockEvaluator, f, x)
-    block = evaluator.block
-    for edge in all_edges(block)
+    # evaluate gradient for each edge and sum
+    f_locals = [spzeros(length(f)) for _ = 1:length(evaluator.block.edges)]
+    edges = all_edges(evaluator.block)
+    Threads.@threads for i = 1:length(edges)
+        edge = edges[i]
         ninds = evaluator.edge_data[edge].ninds
-        MOI.eval_objective_gradient(edge, view(f,ninds), view(x,ninds))
+        MOI.eval_objective_gradient(edge, view(f_locals[i],ninds), view(x,ninds))
     end
+    f[:] = sum(f_locals)
     return nothing
 end
 
 ### Eval_G_CB
 
-# function eval_constraint(evaluator::BlockEvaluator, c, x)
-#     #@blas_safe_threads 
-#     block = evaluator.block
-# 	for edge in all_edges(block)
-# 		ninds = column_inds(block, edge)
-# 		minds = row_inds(block, edge)
-# 		MOI.eval_constraint(edge, view(c, minds), view(x, ninds))
-# 	end
-#     return nothing
+function MOI.eval_constraint(evaluator::BlockEvaluator, c, x)
+	Threads.@threads for edge in all_edges(evaluator.block)
+		ninds = evaluator.edge_data[edge].ninds
+		minds = evaluator.edge_data[edge].minds
+		MOI.eval_constraint(edge, view(c, minds), view(x, ninds))
+	end
+    return nothing
+end
+
+### Eval_Jac_G_CB
+
+
+
+
+# function MOI.jacobian_structure(evaluator::BlockEvaluator)::Vector{Tuple{Int64,Int64}}
+#     I = Vector{Int64}(undef, evaluator.nnz_jac) # row indices
+#     J = Vector{Int64}(undef, evaluator.nnz_jac) # column indices
+
+#     for edge in all_edges(evaluator.block)
+#         edge_data = evaluator.edge_data[edge]
+#         isempty(edge_data.nnzs_jac_inds) && continue
+
+#         offset_i = edge_data.minds[1] - 1
+#         offset_j = edge_data.ninds[1] - 1
+        
+#         II = view(I, edge_data.nnzs_jac_inds)
+#         JJ = view(J, edge_data.nnzs_jac_inds)
+
+#         edge_jacobian_sparsity = MOI.jacobian_structure(edge)
+
+#         for (k,(row,col)) in enumerate(edge_jacobian_sparsity)
+#             II[k+offset_i] = row
+#             JJ[k+offset_j] = col
+#         end
+#     end
+#     jacobian_sparsity = collect(zip(I, J))
+#     return jacobian_sparsity
+# end
+
+
+# function MOI.jacobian_structure(model::Optimizer)
+#     J = MOI.jacobian_structure(model.qp_data)
+#     offset = length(model.qp_data)
+#     if length(model.nlp_data.constraint_bounds) > 0
+#         for (row, col) in MOI.jacobian_structure(model.nlp_data.evaluator)
+#             push!(J, (row + offset, col))
+#         end
+#     end
+#     return J
+# end
+
+# function MOI.eval_constraint_jacobian(model::Optimizer, values, x)
+#     offset = MOI.eval_constraint_jacobian(model.qp_data, values, x)
+#     nlp_values = view(values, (offset+1):length(values))
+#     MOI.eval_constraint_jacobian(model.nlp_data.evaluator, nlp_values, x)
+#     return
+# end
+
+# function MOI.jacobian_structure(d::OptiGraphNLPEvaluator)
+#     nnzs_jac_inds = d.nnzs_jac_inds
+#     I = Vector{Int64}(undef, d.nnz_jac)
+#     J = Vector{Int64}(undef, d.nnz_jac)
+#     #@blas_safe_threads for k=1:length(modelnodes)
+#     for k in 1:length(d.nlps)
+#         isempty(nnzs_jac_inds[k]) && continue
+#         offset_i = d.minds[k][1] - 1
+#         offset_j = d.ninds[k][1] - 1
+#         II = view(I, nnzs_jac_inds[k])
+#         JJ = view(J, nnzs_jac_inds[k])
+#         _jacobian_structure(d.nlps[k], II, JJ)
+#         II .+= offset_i
+#         JJ .+= offset_j
+#     end
+#     jacobian_sparsity = collect(zip(I, J)) # return Tuple{Int64,Int64}[]
+#     return jacobian_sparsity
 # end
 
 # ### Eval_H_CB
